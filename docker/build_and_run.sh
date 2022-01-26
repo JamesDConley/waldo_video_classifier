1cdb7ca95db9e6f564582f4976351bf495c07d42
James Conley <jconley1@conncoll.edu>
2022-01-25 21:01:13 -0500

    Setup first-pass modeling & eda

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -2,6 +2,6 @@ parentdir=$(dirname `pwd`)
 uid=$(id -u ${USER})
 gid=$(id -g ${USER})
 # Include --no-cache if needing to rebuild for a test
-docker build -t "unnamed_project" .
+docker build -t "waldo" .
 
-docker run --gpus all -it --rm --name unnamed_project  -p 8862:8862 -v $parentdir:/module unnamed_project /bin/bash
+docker run --gpus all -it --rm --name waldo  -p 8862:8862 -v $parentdir:/module waldo /bin/bash
